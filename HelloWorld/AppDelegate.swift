5012ccdb996faa5a3b0f4da948241f5a3237db03
Kimsunghyunny <sorkaj123@gmail.com>
2021-08-04 17:04:12 +0900

    commit

--- HelloWorld/AppDelegate.swift
+++ HelloWorld/AppDelegate.swift
@@ -5,6 +5,10 @@
 //  Created by SungHyun Kim on 2021/08/04.
 //
 
+
+// app delegate는 앱 전체의 적용해야 할 기능을 담당하는 클래스이다.
+// 즉, 앱이 실행되고 종료될 때, 그리고 활 성화 상태가 되거나 비활성화 상태가 될때, 백그라운드 상태로 들어갈때 등의 다양한 상태변화를 감지하고 이에 대한 처리를 해줘야 할때 사용하는 클래스이다.
+
 import UIKit
 
 @main
@@ -14,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(5)
         return true
     }
 
--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -5,15 +5,22 @@
 //  Created by SungHyun Kim on 2021/08/04.
 //
 
+// 앱은 하나 이상의 화면을 가지는데, 이를 관리하기 위해 사용되는것. 일반적으로 화면의 개수만큼 뷰 컨트롤러가 필요하며, 해당 파일은 첫번째 화면에서 사용될 뷰 컨트롤러에 해당.
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var uiTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sayHello(_ sender: Any) {
+        self.uiTitle.text = "Hello, World!"
+    }
+    
 }
 
